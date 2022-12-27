fc4e7ebe03d9cfca90a82cc3ea7f2270b6531525
Corvier <victorherrera1316@gmail.com>
2022-12-26 22:35:05 -0600

    Agregando reto

---
+++ JavaScript a profundidad/Curso basico de JavaScript/rockPaperScissors.js
@@ -0,0 +1,27 @@
+let option = "";
+const options = ["piedra", "papel", "tijera"];
+const machine = options[Math.floor(Math.random() * 3)];
+
+function userOption() {
+    option = prompt("Ingresa su opción: piedra, papel o tijera ");
+    return (!options.includes(option.toLowerCase())) ? userOption() : rockPaperScissors(option, machine);
+}
+
+function rockPaperScissors(user, machine) {
+    switch (true) {
+        case (user === machine):
+            console.log('es un empate');
+            break;
+        case (machine === 'piedra' && user === 'papel'):
+            console.log('Ganaste')
+            break;
+        case (machine === 'papel'  && user === 'tijera'):
+            console.log('Ganaste')
+            break;
+        case (machine === 'tijera' && user === 'piedra'):
+            console.log('Ganaste')
+            break;
+        default:
+            console.log('¡Perdiste!');       
+    }
+}
