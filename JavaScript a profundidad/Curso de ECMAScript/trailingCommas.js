41df00c11811cb3327a781b89613082db6acf2b4
Corvier <victorherrera1316@gmail.com>
2022-12-28 22:27:40 -0600

    Agregando uso de Trailing Commas

---
+++ JavaScript a profundidad/Curso de ECMAScript/trailingCommas.js
@@ -0,0 +1,20 @@
+/*
+Trailing commas
+Las trailing commas consisten en comas al final de objetos o arrays que faciliten añadir nuevos elementos y evitar errores de sintaxis.
+
+const usuario = {
+    name: 'Andres',
+    email: "andres@correo.com",
+    age: 23, //<-- Trailing comma
+}
+
+const nombres = [
+    "Andres",
+    "Valeria",
+    "Jhesly", //<-- Trailing comma
+]
+*/
+
+const ages = [24, 45, ,24, 28, 38, 12, 45, , , ];
+console.log("🚀 ~ file: trailingCommas.js:2 ~ ages", ages);
+console.log("🚀 ~ file: trailingCommas.js:20 ~ ages.length", ages.length)
