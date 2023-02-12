74d45b2d0bd5cfb83563955831f2f30a883d825b
Corvier <victorherrera1316@gmail.com>
2023-02-12 17:24:33 -0600

    Instalacion de express y creando servidor http

--- JavaScript a profundidad/3 - Curso de Backend con Nodejs API REST con Expressjs/my-store/src/app.js
+++ JavaScript a profundidad/3 - Curso de Backend con Nodejs API REST con Expressjs/my-store/src/app.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo!");
+const express = require('express');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => res.send('Hello World!'))
+// Starting the server
+app.listen(app.get("port"), () => { console.clear(); console.log(`Server is Running on port http://localhost:${app.get("port")}`); });
