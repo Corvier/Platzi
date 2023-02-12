97f20a45f23c42ddc1b1d3d4040ee3318e837704
Corvier <victorherrera1316@gmail.com>
2023-02-12 17:31:58 -0600

    Creando rutas con express

--- JavaScript a profundidad/3 - Curso de Backend con Nodejs API REST con Expressjs/my-store/src/app.js
+++ JavaScript a profundidad/3 - Curso de Backend con Nodejs API REST con Expressjs/my-store/src/app.js
@@ -4,6 +4,19 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 3000);
 
+// Routes
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/nueva-ruta', (req, res) => {
+    res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+    res.json({
+        name: 'Producto 1',
+        price: 1000,
+    });
+});
+
 // Starting the server
 app.listen(app.get("port"), () => { console.clear(); console.log(`Server is Running on port http://localhost:${app.get("port")}`); });
