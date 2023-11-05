69e572a23629195c0f21f70defa1479f68ab09b1
Corvier <victorherrera1316@gmail.com>
2023-11-05 00:09:39 -0600

    add(file static)

---
+++ JavaScript a profundidad/4 - Curso de Aplicaciones en tiempo real con Socket.io/first_application_of_socketio/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const path = require('node:path');
+const { Server } = require('socket.io');
+
+const PORT = 4000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+
+// Settings
+app.set('port', PORT);
+
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// Puesta en marcha del servidor
+httpServer.listen(app.get('port'), () => {
+    console.clear();
+    console.log(`Listening on port ${app.get('port')}`);
+});
+
+
+// Manejo de conexiones websocket
+io.on('connection', (socket) => {
+    // console.log(socket);
+    // console.log("ID del socket conectado:", socket.id);
+
+    // Para saber cuantos clientes están conectados
+    // console.log("Clientes conectados:", io.engine.clientsCount);
+
+    // Para saber cuando un cliente se desconecta
+    // socket.on('disconnect', () => {
+    //     console.log(`Socket ${socket.id} disconnected`);
+    // });
+});
